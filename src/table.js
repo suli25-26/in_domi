80bc6394b515b110a8c37055058956fb4088e050
Gyenge Elemér <gyenge@valahol>
2026-03-19 13:34:03 +0100

    Kész

--- src/table.js
+++ src/table.js
@@ -1,4 +1,25 @@
+const tbody = document.querySelector('#tbody')
+
 
 employees = [
-    { id: 1, name: 'Erős István', city: "Szeged", salary: 395}
+    { id: 1, name: 'Erős István', city: "Szeged", salary: 395},
+    { id: 2, name: 'Tar Ferenc', city: "Pécs", salary: 392},
+    { id: 3, name: 'Csaló Gábor', city: "Szeged", salary: 398},
+    { id: 4, name: 'Rendes Irén', city: "Pécs", salary: 393},
+    { id: 5, name: 'Fel Áron', city: "Szeged", salary: 397},
 ]
+
+var rows = ''
+employees.forEach(emp => {
+    let row = `
+    <tr>
+        <td>${emp.id}</td>
+        <td>${emp.name}</td>
+        <td>${emp.city}</td>
+        <td>${emp.salary}</td>
+    </tr>
+    `
+    rows += row
+})
+tbody.innerHTML = rows
+
